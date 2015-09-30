7a41bed475c36579c202cfd837eb5a62aa209119
kevadsett <kev.adsett@gmail.com>
2015-09-30 20:48:07 +0100

    Randomise room positions on Start

---
+++ Assets/Scripts/RandomiseRoomsComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomiseRoomsComponent : MonoBehaviour {
+
+	public List<Vector2> Positions;
+	public List<GameObject> Thumbnails;
+
+	void Start () {
+		for (int i = 0; i < Thumbnails.Count; i++) {
+			Vector2 chosenPosition = Positions [Random.Range (0, Positions.Count)];
+			Positions.Remove (chosenPosition);
+			GameObject thumbnail = (GameObject)Instantiate(Thumbnails [i], new Vector3 (chosenPosition.x, chosenPosition.y, 0.0f), new Quaternion());
+			thumbnail.transform.SetParent (gameObject.transform);
+		}
+	}
+}
